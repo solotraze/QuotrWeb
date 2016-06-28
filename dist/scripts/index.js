4c412ac6d6813f3afa294ab4f53a4d23e3ef6c72
Solo Traze <solo@deb7.solohome>
2016-06-28 19:24:08 +0530

    Stock quote returning one time.

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -1,9 +1,17 @@
-var socket = io(); 
+var socket; 
 
 $(document).ready(function(){
-
+  socket = io(); 
+  socket.on('register_code', function(msg) {
+    console.log('Code registered');
+  });
+  socket.on('quote', function(quote) {
+    console.log(quote);
+  });
 });
 
 var getQuote = function(stockCode) {
-  socket.emit('register_code', stockCode);
+  if (socket) {
+    socket.emit('register_code', stockCode);
+  }
 };
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+var quotr = require('./quotr/quotr');
+
 app.use('/', express.static(__dirname+'/html'));
 app.use('/dist', express.static(__dirname+'/dist'));
 
@@ -10,11 +12,23 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname+'/html/index.html');
 });
 
+var curStockCode;
 io.on('connection', function(socket) {
   console.log('A user connected');
 
   socket.on('register_code', function(code) {
+    curStockCode = code;
     console.log('Registered code: ' + code);
+    io.emit('register_code','success');
+
+    quotr.getQuote(code, function(err, quote) {
+      if (err) {
+        console.log('Error in getting quote. Error: '+err);
+      }
+      else {
+        io.emit('quote', quote);
+      }
+    });
   });
 });
 
