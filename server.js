c7eb92de4c568357c59bb4d50076b3f843473cf1
Solo Traze <traze.solo.dev@gmail.com>
2016-06-29 12:32:55 +0530

    Serve and keep refreshing different clients connected

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-
 var quotr = require('./quotr/quotr');
+
+var socketList = []; 
 
 app.use('/', express.static(__dirname+'/html'));
 app.use('/dist', express.static(__dirname+'/dist'));
@@ -12,27 +13,42 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname+'/html/index.html');
 });
 
-var curStockCode;
+var serveSockets = function() {
+  console.log('Number of clients to refresh: ' + socketList.length);
+
+  // Serve the complete batch in one go
+  socketList.forEach(function(socketInfo) {
+    serveSocket(socketInfo.socket, socketInfo.stockCode);
+  });
+};
+
+var serveSocket = function (socket, stockCode) {
+  if (socket.connected) {
+    quotr.getQuote(stockCode, function(err, quote) {
+      if (err) {
+        console.log('Error in getting quote. Error: '+err);
+      }
+      else {
+        socket.emit('quote', quote);
+      }
+    });
+  }
+};
+ 
 io.on('connection', function(socket) {
   console.log('A user connected');
 
   socket.on('register_code', function(code) {
     var thisSocket = this;
-    curStockCode = code;
+    var socketInfo = { socket: thisSocket, stockCode: code };
+    socketList.push(socketInfo);
+
     console.log('Registered code: ' + code);
     thisSocket.emit('register_code','success');
-
-    quotr.getQuote(code, function(err, quote) {
-      if (err) {
-        console.log('Error in getting quote. Error: '+err);
-      }
-      else {
-        thisSocket.emit('quote', quote);
-      }
-    });
   });
 });
 
 server.listen(8080, '192.168.187.130', function() {
+  setInterval(serveSockets, 5000);
   console.log('App started at port 8080.');
 });
